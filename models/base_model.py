b42ce9da1e2575f5ff667bccada5dc6feaaa2c3b
kimaru355 <emmanuelkimaru355@gmail.com>
2023-10-13 23:53:04 -0700

    airbnb clone

--- models/base_model.py
+++ models/base_model.py
@@ -2,15 +2,46 @@
 
 import uuid
 from datetime import datetime
+from models/__init__ import storage
 
 # the base model
+date_format = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel():
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, date_format))
+                elif key == "created_at":
+                    setattr(self, key, datetime.strptime(value, date_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            storage.new()
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
+
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def to_dict(self):
+        my_dict = {}
+        my_dict['__class__'] = self.__class__.__name__
+        for key, value in self.__dict__.items():
+            if key == "updated_at":
+                my_dict[key] = self.updated_at.strftime(date_format)
+            elif key == "created_at":
+                my_dict[key] = self.created_at.strftime(date_format)
+            else:
+                my_dict[key] = value
+        return my_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import json
+
+
+class FileStorage():
+    def __init__(self):
+        self.__file_path = "my_file.json"
+        self.__objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        for key, value in obj.items():
+            setattr(self, self.__objects[key], value)
+
+    def save(self):
+        with open(self.__file_path, 'w') as file:
+            json.dump(self.__object, file)
+
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r') as file:
+                setattr(self, self.__objects, json.loads(file.read())
+        except FileNotFoundError:
+            pass
